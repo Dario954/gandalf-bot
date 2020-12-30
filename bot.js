642da2a167e13f6eea71f97f8a34b18f2972ad30
Dario954 <75027341+Dario954@users.noreply.github.com>
2020-12-30 16:55:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,53 +1,44 @@
+const express = require('express')                                //
+const app = express();                                            //
+const port = 3000                                                 //
+app.get ('/',(req, res)=> res.send('ciao'))                       //
+                                                                  //
+app.listen(port, () =>                                            //
+console.log('Il tuo bot è ascoltato da http://localhost:${port}'))//
+////////////////////////////////////////////////////////////////////
+
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fetch = require("node-fetch");
- 
-
 
 //TOKEN
-client.login(process.env.GANDALFTOKEN);
+client.login(process.env.BOTTOKEN);
 const serverID = process.env.SERVERID;
-
-
-
-// intervallo
-let count = 0;
-setInterval(
-  () =>
-    require("node-fetch")(process.env.URL).then(() =>
-      console.log(`[${++count}] here i pinged ${process.env.URL}`)
-    ),
-  300000
-);
-
-
 
 // modalità attiva
 client.on ( "ready" , readyDiscord )
-function   readyDiscord () {
-  console . log ( "💖" );
+function readyDiscord () {
+console . log ( "💖" );
 }
 
-
 const replies = [
-  "tu non puoi passare",
+"tu non puoi passare",
 ];
 
 client.on("message", gotMessage);
 
 function gotMessage(msg) {
-  if (msg.channel.id == "789805460638531594" && msg.content === "") {
-    // msg.reply('MESSAGGIO');
-    const index = Math.floor(Math.random() * replies.length);
-    msg.channel.send(replies[index]);
-  }
+if (msg.channel.id == "789805460638531594" && msg.content === "") {
+// msg.reply('MESSAGGIO');
+const index = Math.floor(Math.random() * replies.length);
+msg.channel.send(replies[index]);
+}
 }
 
 client.on("message", getMessage);
 
 function getMessage(msg){
-  if (msg.channel.id == "789805460638531594" && msg.content === ""){
-    msg.reply('');
-  }
+if (msg.channel.id == "789805460638531594" && msg.content === ""){
+msg.send('');
+}
 }
